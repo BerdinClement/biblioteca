44744716f4e20f684ca0cb7fe1675bb104c6ef46
Antoinebac435 <antoinebacquet62@gmail.com>
2024-01-29 13:13:52 +0100

    feat(Services) Add CatalogService

---
+++ LibraryManager/Services/Services/CatalogService.cs
@@ -0,0 +1,25 @@
+﻿using BusinessObjects.Entity;
+using BusinessLayer.Catalog;
+
+namespace Services.Services
+{
+    public class CatalogService
+    {
+        private readonly CatalogManager _catalogManager = new CatalogManager();
+
+        public IEnumerable<Book> ShowCatalog()
+        {
+            return _catalogManager.DisplayCatalog();
+        }
+
+        public IEnumerable<Book> ShowCatalog(BookTypes type)
+        {
+            return _catalogManager.DisplayCatalog(type);
+        }
+
+        public Book? FindBook(int id)
+        {
+            return _catalogManager.FindBook(id);
+        }
+    }
+}
