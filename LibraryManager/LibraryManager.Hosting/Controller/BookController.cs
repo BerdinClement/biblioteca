e79bd5e9e35d5cde5ed58d18c69e0171a930960f
Antoinebac435 <antoinebacquet62@gmail.com>
2024-02-12 16:29:43 +0100

    Update BookController.cs

--- LibraryManager/LibraryManager.Hosting/Controller/BookController.cs
+++ LibraryManager/LibraryManager.Hosting/Controller/BookController.cs
@@ -1,23 +1,52 @@
 using BusinessObjects.Entity;
-using DataAccessLayer.Contexts;
 using Microsoft.AspNetCore.Mvc;
 using Services.Services;
 
-namespace LibraryManager.Hosting.Controller;
-
-[ApiController]
-[Route("[controller]")]
-public class BookController : ControllerBase
+namespace LibraryManager.Hosting.Controller
 {
-  private ICatalogService _catalogService;
-  public BookController(ICatalogService catalogService)
-  {
-    _catalogService = catalogService;
-  }
-  [HttpGet]
-  public IEnumerable<Book> GetAll()
-  {
-    return _catalogService.ShowCatalog();
-  }
-  
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+
+        public BookController(ICatalogService catalogService)
+        {
+            _catalogService = catalogService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetAll()
+        {
+            return _catalogService.ShowCatalog();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var book = _catalogService.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpGet("books/{type}")]
+        public IEnumerable<Book> GetByType(string type)
+        {
+            return _catalogService.GetBooksByType(type);
+        }
+
+        [HttpGet("book/topRatedBook")]
+        public IActionResult GetTopRatedBook()
+        {
+            var topRatedBook = _catalogService.GetTopRatedBook();
+            if (topRatedBook == null)
+            {
+                return NotFound();
+            }
+            return Ok(topRatedBook);
+        }
+    }
 }
