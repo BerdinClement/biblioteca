ded01fc7ed52ec3aff150e88327aa1d445932eed
Clément Berdin <clement.berdin@gmail.com>
2024-02-12 15:07:57 +0100

    fix(Contexts): add modelBuilder

--- LibraryManager/DataAccessLayer/Contexts/LibraryContext.cs
+++ LibraryManager/DataAccessLayer/Contexts/LibraryContext.cs
@@ -1,19 +1,37 @@
 using BusinessObjects.Entity;
 using Microsoft.EntityFrameworkCore;
-using Library = Microsoft.Extensions.DependencyModel.Library;
 
 namespace DataAccessLayer.Contexts;
 
 public class LibraryContext : DbContext
 {
     public DbSet<Book> Books { get; set; }
-    //public DbSet<Author> Authors { get; set; }
-
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Library> Libraries { get; set; }
+    
     public LibraryContext(DbContextOptions<LibraryContext> dbContextOptions) : base(dbContextOptions) {}
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Book>().ToTable("book");
-        // modelBuilder.Entity<Author>().ToTable("author");
+        modelBuilder.Entity<Book>()
+            .ToTable("book")
+            .HasOne<Author>(book => book.Author)
+            .WithMany(author => author.Books)
+            .HasForeignKey("id_author");
+        modelBuilder.Entity<Book>()
+            .ToTable("book")
+            .HasMany<Library>(book => book.Libraries)
+            .WithMany(library => library.Books)
+            .UsingEntity(j => j.ToTable("stock"));
+        modelBuilder.Entity<Author>()
+            .ToTable("author")
+            .HasMany<Book>(author => author.Books)
+            .WithOne(book => book.Author)
+            .HasForeignKey("id_author");
+        modelBuilder.Entity<Library>()
+            .ToTable("library")
+            .HasMany<Book>(library => library.Books)
+            .WithMany(book => book.Libraries)
+            .UsingEntity(j => j.ToTable("stock"));
     }
 }
